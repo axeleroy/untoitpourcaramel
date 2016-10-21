17128d717c1f5d9dc1ab1e4d2627678dec9ba07f
axeleroy <blackeco@free.fr>
2016-10-21 13:35:56 +0200

    Chaque ligne d'une description est désormais précédée de `>` et l'URL de l'annonce précède les images

--- trello_module.py
+++ trello_module.py
@@ -51,20 +51,20 @@ def post():
                       "Charges : %s\n\n" \
                       ">%s" % \
                       (annonce.created.strftime("%a %d %b %Y %H:%M:%S"), annonce.rooms, annonce.bedrooms, annonce.charges,
-                       annonce.description)
+                       annonce.description.replace("\n", "\n>"))
 
         card = _list.add_card(title, desc=description)
-
-        card.attach(url=annonce.link)
 
         # On s'assure que ce soit bien un tableau
         if annonce.picture is not None and annonce.picture.startswith("["):
             # Conversion de la chaîne de caractère représentant le tableau d'images en tableau
             for picture in literal_eval(annonce.picture):
                 card.attach(url=picture)
-        # Il n'y a qu'une photo
+            # Il n'y a qu'une photo
         elif annonce.picture is not None and annonce.picture.startswith("http"):
             card.attach(url=annonce.picture)
+
+        card.attach(url=annonce.link)
 
         annonce.posted2trello = True
         annonce.save()
